7b967836b452e78665807b1ac48e64d9a81619cc
Carlos Arranz Sánchez <carranz998@gmail.com>
2022-06-18 12:35:05 +0200

    Implement database preprocessed data grouper

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
-from data_gathering.linear_database_gather import LinearDatabaseGather
+from pathlib import Path
+
+import pandas as pd
+
+from data_gathering.flat_column_database import FlatColumnDatabase
 
 
 if __name__ == '__main__':
-    LinearDatabaseGather()
+    path_database = Path('resources\\first_party_data\\pokemon_database.txt')
+    path_tiers = Path('resources\\first_party_data\\tiers.txt')
+    
+    tiers = pd.read_csv(path_tiers, header=None).squeeze()
+
+    flat_column_database = FlatColumnDatabase(path_database)
+    transposed_groups = flat_column_database.iterate_transposed_groups(s_group_delimiters=tiers)
+
+    for group in transposed_groups:
+        print(group)
